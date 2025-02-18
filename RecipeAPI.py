805030011aab495aa96c34040d17b396770bf2a8
ryan-berry327 <ryanberry480@gmail.com>
2025-02-18 18:09:04 +0000

    Finsihed ingredients input and starting to connect to the api and gather information

--- RecipeAPI.py
+++ RecipeAPI.py
@@ -3,11 +3,13 @@ class RecipeAPI:
    def __init__(self, api_key,base_url,num_recipe):
         self.api_key = '1e4bbc6925e14291bd42452b5a7da089'
         self.base_url = 'https://api.spoonacular.com'
+        self.endpoint_url  = '/recipes/findByIngredients'
         self.num_recipe = num_recipe
 
     # Build the request URL by adding the URL with parameters
     def build_url(self, ingredients):
-        ingredients_str = ",".join(ingredients) 
+        ingredients_str = ",".join(ingredients)
+        url = f"{self.base_url}{self.endpoint_url}?ingredients={ingredients_str}&apiKey={self.api_key}&number={self.num_recipe}"
 
     def search_recipes(ingredients):
         pass
